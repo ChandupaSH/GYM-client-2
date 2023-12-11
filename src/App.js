b990a14cfb348229379cb2032a386da3726972b0
ChandupaSH <chandupash107@gmail.com>
2023-12-11 21:17:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import HomePage from './scenes/HomePage'
+import UserPage from './scenes/UserPage'
+import QRscannerPage from './scenes/QRscannerPage'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/userpage' element={<UserPage/>}/>
+          <Route path='/qr' element={<QRscannerPage/>}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/scenes/QRscannerPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QRscannerPage = () => {
+  return (
+    <div>
+      QR scanner page
+    </div>
+  )
+}
+
+export default QRscannerPage
